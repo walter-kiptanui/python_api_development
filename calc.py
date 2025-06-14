b8a3a1f39bbd73ad6552e46cb8981995b4ad5b7a
walter-kiptanui <walterkiptanui5@gmail.com>
2025-06-14 06:37:51 +0300

    displaying the data in dictionary form

--- calc.py
+++ calc.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -12,8 +20,9 @@ def get_posts():
     return {"data": "Your bible says, your gift will make room for you."}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content :{payload['content']}"}
+def create_posts(new_post : Post):
+    print(new_post.rating)
+    print(new_post.dict())
+    return {"data": "new post"}
     
 
